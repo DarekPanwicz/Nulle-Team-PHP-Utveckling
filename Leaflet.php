e075c22e597df214c21b7723001db2d547a663ff
Filip Lonc <filiplonc@gmail.com>
2018-01-13 11:37:24 +0100

    dodanie ulotki bez dokumentacji, podstawowe własności i metody

---
+++ Leaflet.php
@@ -0,0 +1,45 @@
+<?php
+
+//New declare construct
+declare(strict_type=1);
+
+class Leaflet
+{
+    protected $name;
+    protected $size;
+    protected $colour;
+    protected $paperType;
+
+    public function setName(string $name):void
+    {
+        $this->name = $name;
+    }
+    public function getName():string
+    {
+        return $this->name;
+    }
+    public function setSize(int size):void
+    {
+        $this->size = $size;
+    }
+    public function getSize():int
+    {
+        return $this->size;
+    }
+    public function setColour(string $colour):void
+    {
+        $this->colour = $colour;
+    }
+    public function getColour():string
+    {
+        return $this->colour;
+    }
+    public function setPaperType(string $paperType)
+    {
+        $this->paperType = $paperType;
+    }
+}
+
+
+
+//rozmiar kolory rodzaj papieru 
