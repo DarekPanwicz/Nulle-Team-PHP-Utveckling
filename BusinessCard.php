148a99b318dad53849695c10a0685e2600e0fda4
Darek P <dajmos007@gmail.com>
2018-01-20 12:05:19 +0100

    Adding new functions

--- BusinessCard.php
+++ BusinessCard.php
@@ -5,10 +5,38 @@ declare(strict_type=1);
 
 
 // New class added
-class BusinessCard
+class BusinessCard extends AbstractProduct
 {
+    // size, string
+   private $size;
+    //color, string
+    private $color;
+    // one page
+    private $numberOfPages;
 
+    /**
+     * function get color
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
 
+    /**
+     * function get size
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * function get number of pages
+     */
+    public function getNumberOfPages()
+    {
+        return $this->numberOfPages;
+    }
 
 
 };
